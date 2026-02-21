3c86fd56611ba22e0924f0ed96f2c98efd547825
ghost <ghst-0@users.noreply.github.com>
2026-02-21 09:04:58 +0700

    Remove failureMessage

--- messages/index.js
+++ messages/index.js
@@ -1,10 +1,8 @@
-import failureMessage from './failure_message.js';
 import fundsSummary from './funds_summary.js';
 import liquiditySummary from './liquidity_summary.js';
 import settleTradeMessage from './settle_trade_message.js';
 
 export {
-  failureMessage,
   fundsSummary,
   liquiditySummary,
   settleTradeMessage
