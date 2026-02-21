a7a59f459ff6a14460491cde04297991129a9c74
ghost <ghst-0@users.noreply.github.com>
2026-02-21 09:00:24 +0700

    Remove handleEditedMessage

--- commands/index.js
+++ commands/index.js
@@ -2,7 +2,6 @@ import handleBalanceCommand from './handle_balance_command.js';
 import handleConnectCommand from './handle_connect_command.js';
 import handleCostsCommand from './handle_costs_command.js';
 import handleEarningsCommand from './handle_earnings_command.js';
-import handleEditedMessage from './handle_edited_message.js';
 import handleInfoCommand from './handle_info_command.js';
 import handleLiquidityCommand from './handle_liquidity_command.js';
 import handleMempoolCommand from './handle_mempool_command.js';
@@ -16,7 +15,6 @@ export {
   handleConnectCommand,
   handleCostsCommand,
   handleEarningsCommand,
-  handleEditedMessage,
   handleInfoCommand,
   handleLiquidityCommand,
   handleMempoolCommand,
